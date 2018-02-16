1d7e81f389c2e913a41f89171353ca9370da6026
Tristan Price <33553882+tri-llionaire@users.noreply.github.com>
2018-02-16 10:06:50 -0500

    Add files via upload

---
+++ triple/1.x.x/triple_1.0.1.py
@@ -0,0 +1,23 @@
+#TRIPLE-LANG
+print 'WELCOME TO THE TRIPLE CONSOLE\n'
+while 1 == 1:
+    dic = []
+    commands = {'PRI': 'print', 'END': 'exit()'}
+    string = ''
+    usrinput = raw_input()
+    usrarray = usrinput.split()
+    x = 0
+    while len(dic) < len(usrarray):
+        dic.append(usrarray[x])
+        x += 1
+    if dic[0] == 'END':
+        break
+    else:
+        string = ' \''
+        command = ''
+        string += str(dic[1])
+        command = commands[dic[0]]
+        string += '\''
+        torun = command + string
+        exec(torun)
+exit()
